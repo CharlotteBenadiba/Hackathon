581f7a26b8527474c9e5d0412108a9e6cac159f4
Ryan Kopping <ryko006@gmail.com>
2024-05-28 11:04:28 -0700

    Menu option 1&2 update

--- main.py
+++ main.py
@@ -8,36 +8,34 @@ import argparse
 def menu():
 
     print('Welcome to MovieMate!')
-    user_choice=input('1.Search for a movie\n2.Browse genres\n3.Get recommendations\n4.Exit\n>>>')
+    user_choice=input('1.Search for a movie\n2.Browse genres\n3.Get recommendations\n4.Exit\n>')
     while user_choice not in ('1234'):
         user_choice=('Invalid input')
     else:
         if user_choice=='1':
             return 1
-        elif user_choice==2:
+        elif user_choice=='2':
             return 2
-        elif user_choice==3:
+        elif user_choice=='3':
             return 3
-        elif user_choice==4:
+        elif user_choice=='4':
             return 4
         
 def movie_search():
     movie=input('\nType the movie name\n\n')
     result=movie_api.fetch_movie_data(movie)
-    if result is not None:
-        for key,value in result.items():
-            print(f'{key}: {value}')
-    else:
-            print('No movie found. Check spelling')
+    return result
+
+def browse_genre ():
+    genre=input('\nType the genre you want\n\n')
+    result = movie_api.fetch_movie_by_genre(genre)
+    return result
+
+#Need to add in the database code
+def watchlist(search_movie):
+    query=(f'insert into watchlist values{search_movie}')
+    print ('Success')
     
-
-def browse_genre (genre):
-    movie=input('\nType the genre you want\n\n')
-    result = movie_api.fetch_movie_data(genre)
-
-
-
-    pass
     
 def main():
     # # Инициализация базы данных
@@ -67,7 +65,17 @@ def main():
     user_input=menu()
     while user_input != 4:
         if user_input==1:
-            movie_search()
+            movie=movie_search()
+            if movie is not None:
+                if movie is not None:
+                    for key,value in movie.items():
+                        print(f'{key}: {value}')
+                else:
+                    print('No movie found. Check spelling')
+
+            save = input('\nSave to watchlist?Y/N\n')
+            if save.lower()=='y':
+                watchlist(movie)
             search_again = input('\nSearch again? Y/N\n')
             while search_again.lower()=='y':
                 print()
@@ -76,12 +84,22 @@ def main():
             else: 
                 print()
                 user_input=menu()
+                
         elif user_input==2:
-            browse_genre()
+            genre_list=browse_genre()
+
+            if genre_list is not None:
+              
+                for key,value in genre_list.items():
+                        print(f'{key}: {value}')
+                
+            else:
+                print('No genre found. Check spelling')
+
             search_again = input('\nSearch again? Y/N\n')
             while search_again.lower()=='y':
                 print()
-                browse_genre()
+                genre_list=browse_genre()
                 search_again=input('\nSearch again? Y/N\n')
             else:
                 print()
--- movie_api.py
+++ movie_api.py
@@ -1,39 +1,50 @@
 import requests
+import random
 
 API_KEY = '4eb1b83459cbe02d93974fbe71bae0ac'
 BASE_URL = 'https://api.themoviedb.org/3'
+genre_dict={28: 'Action', 12: 'Adventure', 16: 'Animation', 35: 'Comedy', 80: 'Crime', 99: 'Documentary', 18: 'Drama', 10751: 'Family', 14: 'Fantasy', 36: 'History', 27: 'Horror', 10402: 'Music', 9648: 'Mystery', 10749: 'Romance', 878: 'Science Fiction', 10770: 'TV Movie', 53: 'Thriller', 10752: 'War', 37: 'Western'}
 
 def fetch_movie_data(movie_name):
     url = f"{BASE_URL}/search/movie?api_key={API_KEY}&query={movie_name}"
     response = requests.get(url)
-    return parse_info(response.json())
+    return parse_info(response.json(),'search')
+
+def fetch_movie_by_genre(user_genre):
+    genre_id=''
+    for code,genre in genre_dict.items():
+        if genre.lower()==user_genre:
+            genre_id=code
+    url = f"{BASE_URL}/discover/movie?api_key={API_KEY}&with_genres={genre_id}"
+    response = requests.get(url)
+    return parse_info(response.json(),'genre')
     
 def fetch_movie_details(movie_id):
     url = f"{BASE_URL}/movie/{movie_id}?api_key={API_KEY}"
     response = requests.get(url)
     return parse_info(response.json())
 
-def parse_info(raw_info):
+def parse_info(raw_info,source):
     if raw_info['results']:
-        first_result = raw_info['results'][0]
+        movies=[]
+        for info in raw_info['results']:
+            #get just the names of the genres without the codes
+            genre__names = [genre_dict[genre] for genre in info['genre_ids']]
+            genre_string=', '.join(genre__names)
+            movie_info = {
+                'Title': info.get('title'),
+                'Release_date': info.get('release_date'),
+                'Overview': info.get('overview'),
+                'Genres': genre_string,
+                'Rating': info.get('vote_average'),
+                'Number of ratings': info.get('vote_count')
+            }
+            movies.append(movie_info)
 
-        #creates dictionary of the genres
-        genre_dict={28: 'Action', 12: 'Adventure', 16: 'Animation', 35: 'Comedy', 80: 'Crime', 99: 'Documentary', 18: 'Drama', 10751: 'Family', 14: 'Fantasy', 36: 'History', 27: 'Horror', 10402: 'Music', 9648: 'Mystery', 10749: 'Romance', 878: 'Science Fiction', 10770: 'TV Movie', 53: 'Thriller', 10752: 'War', 37: 'Western'}
-        #get just the names of the genres without the codes
-        genre__names = [genre_dict[genre] for genre in first_result['genre_ids']]
-        #format the genres to display nicely
-        genre_string=', '.join(genre__names)
-
-        #format the info for the movie so it is more readable
-        movie_info = {
-            'Title': first_result.get('title'),
-            'Release_date': first_result.get('release_date'),
-            'Overview': first_result.get('overview'),
-            'Genres': genre_string,
-            'Rating': first_result.get('vote_average'),
-            'Number of ratings': first_result.get('vote_count')
-        }
-        return movie_info
+        if source=='search':
+            return movies[0]
+        elif source=='genre':
+            return random.choice(movies)
     else:
         return None
  
